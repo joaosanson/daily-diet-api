56d9bdcff3a2fa97996605f9b13e6426c49d4b53
João Sanson <joaosanson01@gmail.com>
2024-03-01 17:21:42 -0300

    feat: starting project folders

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
-import { env } from 'process'
+import { env } from './env'
 import { app } from './app'
-import 'dotenv/config'
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log(`Server is running at port ${env.PORT}`)
