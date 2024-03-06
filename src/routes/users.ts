174d6c3fb644b016cef34b919a5f52c3f500822d
Joao Victor Sanson <ja36776@malwee.com.br>
2024-03-05 21:25:15 -0300

    feat: adding users route

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,7 +1,14 @@
 import { FastifyInstance } from 'fastify'
+import { knex } from '../database'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.get('/', async (request, reply) => {
-    reply.send()
+    const user = await knex('users').select()
+
+    if (user.length === 0) {
+      throw new Error('User not found.')
+    }
+
+    reply.send(user)
   })
 }
