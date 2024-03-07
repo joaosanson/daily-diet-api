777824dacd5c552d42fc82a38af1da447793c817
João Sanson <joaosanson01@gmail.com>
2024-03-06 21:43:45 -0300

    feat: add user route post

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,5 +1,7 @@
 import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
+import { z } from 'zod'
+import { hash } from 'bcrypt'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.get('/', async (request, reply) => {
@@ -10,5 +12,30 @@ export async function usersRoutes(app: FastifyInstance) {
     }
 
     reply.send(user)
+  })
+
+  app.post('/', async (request, reply) => {
+    const createUsersBodySchema = z.object({
+      name: z.string(),
+      email: z.string(),
+      password: z.string(),
+    })
+
+    const { name, email, password } = createUsersBodySchema.parse(request.body)
+    const hashedPassword = await hash(password, 8)
+    const user = await knex('users').select().where({ email })
+
+    if (user.length !== 0) {
+      throw new Error('Email already in use.')
+    }
+
+    await knex('users').insert({
+      id: crypto.randomUUID(),
+      name,
+      email,
+      password: hashedPassword,
+    })
+
+    reply.status(201).send({ message: 'User created successfully!' })
   })
 }
