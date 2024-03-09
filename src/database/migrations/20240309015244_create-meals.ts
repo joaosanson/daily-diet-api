646c426b0246e6312da2a8227590c33cc183d829
João Sanson <joaosanson01@gmail.com>
2024-03-09 20:38:10 -0300

    feat: add migrations

--- src/database/migrations/20240309015244_create-meals.ts
+++ src/database/migrations/20240309015244_create-meals.ts
@@ -1,5 +1,16 @@
 import type { Knex } from 'knex'
 
-export async function up(knex: Knex): Promise<void> {}
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('meals', (table) => {
+    table.uuid('id').primary()
+    table.text('user_id').references('id').inTable('users').onDelete('CASCADE')
+    table.text('name').notNullable()
+    table.text('description').notNullable()
+    table.date('date').notNullable()
+    table.boolean('isOnDiet').notNullable()
+  })
+}
 
-export async function down(knex: Knex): Promise<void> {}
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('meals')
+}
