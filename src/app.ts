7d47fa613696c6e152d1e1d324591b4225ed155f
João Sanson <joaosanson01@gmail.com>
2024-03-10 21:14:55 -0300

    feat: add auth jwt token

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,17 @@
 import fastify from 'fastify'
 import { usersRoutes } from './routes/users'
+import { mealsRoutes } from './routes/meals'
+import { sessionRoutes } from './routes/session'
 
 export const app = fastify()
 
 app.register(usersRoutes, {
   prefix: 'users',
+})
+
+app.register(mealsRoutes, {
+  prefix: 'meals',
+})
+app.register(sessionRoutes, {
+  prefix: 'session',
 })
---
+++ src/routes/session.ts
@@ -0,0 +1,37 @@
+import { FastifyInstance } from 'fastify'
+import { auth } from '../config/auth'
+import { z } from 'zod'
+import { knex } from '../database'
+import { compare } from 'bcrypt'
+import { sign } from 'jsonwebtoken'
+
+export async function sessionRoutes(app: FastifyInstance) {
+  app.post('/', async (request, reply) => {
+    const createBodySchema = z.object({
+      email: z.string(),
+      password: z.string(),
+    })
+
+    const { email, password } = createBodySchema.parse(request.body)
+
+    const user = await knex('users').select().where({ email }).first()
+
+    if (!user) {
+      throw Error('Email and/or password incorrect.')
+    }
+
+    const passwordMatched = await compare(password, user.password)
+
+    if (!passwordMatched) {
+      throw Error('Email and/or password incorrect.')
+    }
+
+    const { secret, expiresIn } = auth.jwt
+    const token = sign({}, secret, {
+      subject: String(user.id),
+      expiresIn,
+    })
+
+    reply.send({ user, token })
+  })
+}
