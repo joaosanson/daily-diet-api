68866131919971c5b2e84cd9a66d423a0450665c
João Sanson <joaosanson01@gmail.com>
2024-03-13 21:22:42 -0300

    feat: start meals route

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -1,7 +1,40 @@
 import { FastifyInstance } from 'fastify'
+import { ensureAuth } from '../middlewares/ensureAuth'
+import { CustomFastifyRequest } from '../@types/fastify'
+import { z } from 'zod'
+import { knex } from '../database'
 
 export async function mealsRoutes(app: FastifyInstance) {
-  app.post('/', async (request, reply) => {
+  app.addHook('preHandler', ensureAuth)
+
+  app.post('/', async (request: CustomFastifyRequest, reply) => {
+    const getMealsQuerySchema = z.object({
+      name: z.string(),
+      description: z.string(),
+      dateTime: z.string().datetime(),
+      isOnDiet: z.boolean(),
+    })
+
+    const getUserSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id: userId } = getUserSchema.parse(request.user)
+
+    const { name, description, dateTime, isOnDiet } = getMealsQuerySchema.parse(
+      request.query,
+    )
+
+    const mealsId = crypto.randomUUID()
+
+    await knex('meals').insert({
+      id: mealsId,
+      user_id: userId,
+      name,
+      description,
+      date: dateTime,
+      isOnDiet,
+    })
     reply.send()
   })
 }
